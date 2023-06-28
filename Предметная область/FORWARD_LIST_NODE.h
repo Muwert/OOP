51e21174fffd67bb1d5b5cb11f9bd788dce8f53d
Ksenia Egorova <118151597+Muwert@users.noreply.github.com>
2023-06-29 03:43:54 +1000

    FORWARD_LIST_NODE.h

--- Предметная область/FORWARD_LIST_NODE.h
+++ Предметная область/FORWARD_LIST_NODE.h
@@ -28,38 +28,3 @@ namespace Container
 }
 
 #endif 
-/**
-
-#ifndef FORWARD_LIST_CONTAINER_FORWARD_LIST_H
-#define FORWARD_LIST_CONTAINER_FORWARD_LIST_H
-#include<memory>
-
-namespace Container 
-{
-    template <typename T>
-    class ForwardList;
-
-    template<typename T>
-    class ForwardList_Iterator;
-
-    template<typename T>
-    class ForwardList_Node 
-    {
-        friend class ForwardList_Iterator<T>;
-        friend class ForwardList<T>;
-    public:
-        using value_type = T;
-        using reference = value_type&;
-        using const_reference = const value_type&;
-        using _forward_list_node = typename Container::ForwardList_Node<T>;
-        using _node_pointer = typename Container::ForwardList_Node<T>*;
-
-    private:
-        value_type value;
-        _node_pointer next;
-        explicit ForwardList_Node(const_reference value = value_type{}, _node_pointer next = nullptr) : value(value), next(next) {};
-        ~ForwardList_Node() = default;
-    };
-}
-
-#endif */
